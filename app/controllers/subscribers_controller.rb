a934110bd95a75722bc0f8a0bf413e0a48c07f51
Sergey Sintyalev <sintyalev@mail.ru>
2012-07-23 16:56:06 +0400

    Модификация формы постановки под контроль нового абонента.

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,2 +1,14 @@
 class SubscribersController < ApplicationController
+  def new
+    @subscriber = Subscriber.new
+  end
+
+  def create
+    @subscriber = Subscriber.new(params[:subscriber])
+    if @subscriber.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render "new"
+    end
+  end
 end
