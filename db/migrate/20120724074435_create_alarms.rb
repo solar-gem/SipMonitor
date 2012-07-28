7860b21dc5cc8928ecef8e8b75db40c4b976d50f
S.Sintyalev_dom <solar-gem@mail.ru>
2012-07-28 07:13:53 +0400

    Редактирование миграций. Чистка.

--- db/migrate/20120724074435_create_alarms.rb
+++ db/migrate/20120724074435_create_alarms.rb
@@ -1,10 +1,12 @@
 class CreateAlarms < ActiveRecord::Migration
   def change
     create_table :alarms do |t|
-      t.datetime :alarm_raised_time
-      t.datetime :cleared_time
-      t.text :data
-      t.references :subscriber
+      t.string :serial_number, :limit => 10, :null => false # Серийный номер аварии (номер по порядку)
+      t.datetime :alarm_raised_time, :null => false        # Время начала аварии
+      t.datetime :cleared_time                             # Время восстановления аварии
+      t.text :data                                         # Текст аврийного сообщения
+      t.references :subscriber, :null => false             # Ссылка на таблицу с номером абонента
+      t.boolean :status, :default => true                  # Статус аварии, активна или история
 
       t.timestamps
     end
