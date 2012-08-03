2fd0600c2b850986c13aff09f0fa481dd142e8d6
Sergey Sintyalev <sintyalev@mail.ru>
2012-08-03 11:18:52 +0400

    Оптимизация вычисления данных для графика статистики.

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -3,12 +3,30 @@ class Alarm < ActiveRecord::Base
   attr_accessible :alarm_raised_time, :cleared_time, :data
 
 
-  def statistics
+  def self.statistics
      time_now = Time.now
      year = time_now.year
      month = time_now.month
      day = time_now.day
-  Alarm.where(:alarm_raised_time => (Time.utc(time_now.year, time_now.month, time_now.day, t1))..Time.utc(time_now.year, time_now.month, time_now.day, t2)).count
-  
+     hour = time_now.hour
+     
+     alarm_all_count = []
+     alarm_count = []
+     alarm_cleared_count = []
+
+     (0..hour).each do |hour_tmp|
+        alarm_all = Alarm.where(:alarm_raised_time => (Time.utc(year, month, day, 0))..Time.utc(year, month, day, hour_tmp + 1)).count
+        cleared_all = Alarm.where(:cleared_time => (Time.utc(year, month, day, 0))..Time.utc(year, month, day, hour_tmp + 1)).count
+        alarm_all_count << (alarm_all - cleared_all)
+
+        alarm_count << Alarm.where(:alarm_raised_time => (Time.utc(year, month, day, hour_tmp))..Time.utc(year, month, day, hour_tmp + 1)).count
+
+        alarm_cleared_count << Alarm.where(:cleared_time => (Time.utc(year, month, day, hour_tmp))..Time.utc(year, month, day, hour_tmp + 1)).count
+     end
+
+
+     {alarm_all_count: alarm_all_count, alarm_count: alarm_count, alarm_cleared_count: alarm_cleared_count} 
+
+ 
   end
 end
